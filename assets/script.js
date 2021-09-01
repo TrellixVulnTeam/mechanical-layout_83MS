a2925af0052e55d65bc1814025cd3d3b2558d341
edymatimbe <edymatimbe@gmail.com>
2021-09-02 00:14:36 +0200

    Adding accordion vaniila js

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,12 @@
-document.querySelector(".faq__question").addEventListener("click", function () {
-  document.querySelector(".faq__answer").classList.toggle("hidden");
-});
+const accordionQuestion = document.querySelectorAll(".faq__question");
+const accordionAnswer = document.querySelectorAll(".faq__answer");
+
+// accordionQuestion.addEventListener("click", function () {
+//   for (let i = 0; i < accordionQuestion.length; i++)
+//     console.log(accordionAnswer[i]);
+// });
+
+// accordionQuestion.addEventListener("click", function () {
+//   let par = this.parentNode;
+//   console.log(par);
+// });
