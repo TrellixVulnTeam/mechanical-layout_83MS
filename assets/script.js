e436804eb4918c588c8490f58c854ad8ad992412
edymatimbe <edymatimbe@gmail.com>
2021-09-02 02:05:21 +0200

    Accorrdion JS done

--- assets/script.js
+++ assets/script.js
@@ -1,8 +1,16 @@
+// Accordion
 const accordionQuestion = document.querySelectorAll(".faq__question");
 const accordionAnswer = document.querySelectorAll(".faq__answer");
 
 for (let i = 0; i < accordionQuestion.length; i++) {
   accordionQuestion[i].addEventListener("click", function () {
-    accordionAnswer[i].classList.toggle("faq__answeropen");
+    if (accordionAnswer[i].classList.contains("faq__answeropen")) {
+      accordionAnswer[i].classList.remove("faq__answeropen");
+    } else {
+      for (let i = 0; i < accordionAnswer.length; i++) {
+        accordionAnswer[i].classList.remove("faq__answeropen");
+      }
+      accordionAnswer[i].classList.add("faq__answeropen");
+    }
   });
 }
